a944363cd3a1c39a05633531a030a8924d8cd0d0
Diego <diego.nicolas.gandara@gmail.com>
2021-09-16 09:47:48 -0300

    Little modularization: move the empleador controllers to own folder

--- controllers/empleador.js
+++ controllers/empleador.js
@@ -1,92 +1,39 @@
-import Playlist from '../models/empleadores';
+const service = require('../models/empleadores');
 
-//Acá vamos a armar el CRUD. Esta funcion METE directamente a la BDD
+const getAllEmployers = (req, res) =>
+  service
+    .getEmployers()
+    .then((response) => res.json(response))
+    .catch((e) => res.json({ e }));
 
-//CREATE DATA/NEW PLAYLIST
-export async function getAllEmployers(req, res) {
-  try {
-  } catch (error) {}
-}
+const getSingleEmployer = (req, res) => {
+  service
+    .getEmployers({ dni: req.params.dni })
+    .then((response) => res.json(response))
+    .catch((e) => res.json({ e }));
+};
 
-//READ/GET ALL
-export async function getAllPlaylists(req, res) {
-  try {
-    const playlists = await Playlist.findAll();
-    res.status(200).json({
-      data: playlists,
-    });
-  } catch (error) {
-    console.log('El error es:', error);
-    res.status(500).json({
-      message: 'Something gone wrong getting all the  Playlist',
-      data: {},
-    });
-  }
-}
+const createEmployer = (req, res) => {
+  const { dni, nombre, apellido } = req.body;
+  console.log(`dni`, dni, 'nombre', nombre, 'apellido', apellido);
+  return service
+    .newEmployer({ dni, nombre, apellido })
+    .then((response) => res.json(response))
+    .catch((e) => res.json({ e }));
+};
 
-// READ / GET 1 by ID
+const updateEmployer = (req, res) => {
+  const { dni, nombre, apellido } = req.body;
+  console.log(`dni`, dni, 'nombre', nombre, 'apellido', apellido);
+  return service
+    .updateEmployer({ dni }, { dni, nombre, apellido })
+    .then((response) => res.json(response))
+    .catch((e) => res.json({ e }));
+};
 
-export async function getOnePlaylist(req, res) {
-  try {
-    const { id } = req.params;
-    const queryPlaylist = await Playlist.findOne({
-      where: {
-        id,
-      },
-    });
-    res.status(200).json(queryPlaylist);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-}
-
-//DELETE BY ID
-export async function deleteOnePlaylist(req, res) {
-  try {
-    const { id } = req.params;
-    const deleteRowCount = await Playlist.destroy({
-      where: {
-        id,
-      },
-    });
-    res.status(200).send({
-      message: 'Playlist deleted succesfully',
-      count: deleteRowCount,
-    });
-  } catch (error) {
-    res.status(500).send(error);
-  }
-}
-
-// UPDATE BY ID
-export async function updateOnePlaylist(req, res) {
-  try {
-    const { id } = req.params;
-    const { name, priority, description, deliverydate } = req.body;
-
-    const playlists = await Playlist.findAll({
-      attributes: ['id', 'name', 'description', 'deliverydate'],
-      where: {
-        id,
-      },
-    });
-    if (playlists.length > 0) {
-      playlists.forEach(async (playlist) => {
-        await playlist.update({
-          name,
-          priority,
-          description,
-          deliverydate,
-        });
-      });
-    }
-
-    return res.status(200).json({
-      message: 'Playlist updated succesfully',
-      data: playlists,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
-  }
-}
+module.exports = {
+  getAllEmployers,
+  getSingleEmployer,
+  createEmployer,
+  updateEmployer,
+};
--- routes/empleador.js
+++ routes/empleador.js
@@ -1,41 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const service = require('../models/empleadores');
-
-const getAllEmployers = (req, res) =>
-  service
-    .getEmployers()
-    .then((response) => res.json(response))
-    .catch((e) => res.json({ e }));
+const {
+  getAllEmployers,
+  getSingleEmployer,
+  createEmployer,
+  updateEmployer,
+} = require('../controllers/empleador');
 
 router.get('/all', getAllEmployers);
-const getSingleEmployer = (req, res) => {
-  service
-    .getEmployers({ dni: req.params.dni })
-    .then((response) => res.json(response))
-    .catch((e) => res.json({ e }));
-};
 
 router.get('/:dni', getSingleEmployer);
 
-const createEmployer = (req, res) => {
-  const { dni, nombre, apellido } = req.body;
-  console.log(`dni`, dni, 'nombre', nombre, 'apellido', apellido);
-  return service
-    .newEmployer({ dni, nombre, apellido })
-    .then((response) => res.json(response))
-    .catch((e) => res.json({ e }));
-};
 router.post('/', createEmployer);
-
-const updateEmployer = (req, res) => {
-  const { dni, nombre, apellido } = req.body;
-  console.log(`dni`, dni, 'nombre', nombre, 'apellido', apellido);
-  return service
-    .updateEmployer({ dni }, { dni, nombre, apellido })
-    .then((response) => res.json(response))
-    .catch((e) => res.json({ e }));
-};
 
 router.put('/:dni', updateEmployer);
 module.exports = router;
